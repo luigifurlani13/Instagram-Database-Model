d26586f3ccf15c8e612e76a5b07ad24fa2ebc2bf
luigifurlani13 <luis.furlan@icloud.com>
2021-10-12 14:45:31 +0000

    started and finished product

--- src/models.py
+++ src/models.py
@@ -14,6 +14,10 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    nickname = Column(String(250), nullable=False)
+    website = Column(String(250), nullable=False)
+    biography = Column(String(500), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -24,6 +28,35 @@ class Address(Base):
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    location = Column(String(250), nullable=False)
+    image = Column(String(250), nullable=False)
+    likes = Column(String(250), nullable=False)
+    comments = Column(String(250), nullable=False)
+    message = Column(String(500), nullable=False)
+    favorite = Column(String(250), nullable=False)
+    person = relationship(Person)
+
+    def to_dict(self):
+        return {}
+
+class Search(Base):
+    __tablename__ = 'search'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    profile = Column(String(250), nullable=False)
+    favorite = Column(String(250), nullable=False)
+    search_bar = Column(String(250), nullable=False)
     person = relationship(Person)
 
     def to_dict(self):
